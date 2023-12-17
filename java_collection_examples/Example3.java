a71aa271685fe3215c922a75de598864568da997
Dipak Wani <126748063+2001204@users.noreply.github.com>
2023-12-17 20:27:16 +0530

    Add files via upload

---
+++ java_collection_examples/Example3.java
@@ -0,0 +1,24 @@
+
+//What is the final variable, final class, and final blank variable?
+// 1. Final Variable:
+// 2. Blank final variable:
+// 3. Final Method:
+
+public class Example3 {
+    // final int MAX_VALUE;
+
+    // happy
+    // Example3(){
+    //     MAX_VALUE = 100;
+    // }
+
+    // not happy
+    // void myMethod() {
+    //     MAX_VALUE = 101;
+    // }
+
+
+    public static void main(String[] args) { 
+    }
+
+}
